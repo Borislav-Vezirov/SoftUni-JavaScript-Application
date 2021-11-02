50d578c477a04c766973e44e642d06cfc2a4e5d8
Borislav Vezirov <bobi.vez@gmail.com>
2021-11-02 12:00:56 +0200

    New folder and tasks are created

--- Asynchronous Programming/05. Accordion/accordion.js
+++ Asynchronous Programming/05. Accordion/accordion.js
@@ -1,3 +1,53 @@
-function solution() {
-    //TODO .....
+async function solution() {
+
+    let main = document.querySelector('#main');
+
+    let url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    for (const obj of data) {
+        //made with the innerHTML method, because the task is very simple, and this is not so important!
+        main.innerHTML += `
+            
+            <div class="accordion">
+                <div class="head">
+                    <span>${obj.title}</span>
+                    <button class="button" id=${obj._id}>More</button>
+                </div>
+                <div class="extra">
+                    <p></p>
+                </div>
+            </div> `
+    }
+
+    main.addEventListener('click', showDetails);
+
+    async function showDetails(e){
+        if(e.target.className !== 'button'){
+            return;
+        }
+        
+        let urlDetails = `http://localhost:3030/jsonstore/advanced/articles/details/${e.target.id}`
+
+        let detailRes = await fetch(urlDetails);
+
+        let detailData = await detailRes.json();
+
+        let p = e.target.parentElement.parentElement.querySelector('p');
+        p.textContent = detailData.content;
+
+        let divExtra = e.target.parentElement.parentElement.querySelector('.extra');
+
+        if(e.target.textContent == 'More'){
+            e.target.textContent   = 'Less';
+            divExtra.style.display = 'block';
+        }else if(e.target.textContent == 'Less'){
+            e.target.textContent   = 'More';
+            divExtra.style.display = 'none';
+        }
+    }
 }
+
+solution();
--- Asynchronous Programming/06.Blog/app.js
+++ Asynchronous Programming/06.Blog/app.js
@@ -1,5 +1,90 @@
 function attachEvents() {
-    console.log('TODO...');
+    
+    let loadBtn        = document.querySelector('#btnLoadPosts');
+    let viewPostsBtn   = document.querySelector('#btnViewPost');
+    
+    loadBtn.addEventListener('click', loadPosts);
+    
+    viewPostsBtn.addEventListener('click', viewPosts);
+}
+
+async function viewPosts(){
+    
+    let selectedOption = document.querySelector('#posts').value;
+    
+    let postTitle      = document.querySelector('#post-title');
+    let postBodyUl     = document.querySelector('#post-body');
+    let postCommentsUl = document.querySelector('#post-comments');
+
+    let [post, comments] = await Promise.all( [getPost(selectedOption), getComments(selectedOption)] );
+
+    postTitle.textContent = 'Loading...'
+    postBodyUl.textContent = '';  
+    postCommentsUl.replaceChildren();
+
+    setTimeout(() => {
+
+        postTitle.textContent = (post.title).toUpperCase();
+        let liEl = document.createElement('li');
+        liEl.textContent = post.body;
+        postBodyUl.appendChild(liEl);
+    
+        for (const comment of comments) {
+            let createLi = document.createElement('li');
+            createLi.id = comment.id;
+            createLi.textContent = comment.text;
+            postCommentsUl.appendChild(createLi);
+        }
+    }, 650);
+    
+}
+
+async function loadPosts(){
+
+    let selectEl = document.querySelector('#posts')
+
+    selectEl.textContent = '';
+
+    let data = await getPosts();
+    
+    for (const post of data) {
+        let optionEl = document.createElement('option');
+        optionEl.textContent = (post.title).toUpperCase();
+        optionEl.value = post.id;
+        selectEl.appendChild(optionEl);
+    }
+}
+
+async function getPosts(){
+
+    let url   = 'http://localhost:3030/jsonstore/blog/posts';
+
+    let res   = await fetch(url);
+    let data  = await res.json();
+    let posts = Object.values(data)
+    
+    return posts;
+}
+
+async function getPost(postId){
+
+    let url   = 'http://localhost:3030/jsonstore/blog/posts/' + postId;
+
+    let res   = await fetch(url);
+    let data  = await res.json();
+    
+    return data;
+}
+
+async function getComments(postId){
+
+    let url     = 'http://localhost:3030/jsonstore/blog/comments';
+
+    let res     = await fetch(url);
+    let data    = await res.json();
+    let comment = Object.values(data).filter(x => x.postId == postId);
+
+    return comment;
 }
 
 attachEvents();
