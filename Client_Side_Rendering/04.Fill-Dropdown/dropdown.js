b707a02238da6720bc682170877c3e74af9a6360
Borislav Vezirov <bobi.vez@gmail.com>
2021-11-19 22:18:08 +0200

    In proccess to solving th task and make new folder

--- Client_Side_Rendering/04.Fill-Dropdown/dropdown.js
+++ Client_Side_Rendering/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,43 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const selectEl = document.getElementById('menu');
+document.querySelector('form').addEventListener('submit', onSubmit)
+
+const selectTemplate = (items) => html `
+
+    ${items.map(x => html `<option value="${x._id}">${x.text}</option>`)}
+`
+getData();
+
+async function getData(){
+
+    const res = await fetch('http://localhost:3030/jsonstore/advanced/dropdown');
+
+    const data = await res.json();
+
+    update(Object.values(data));
+}
+
+async function onSubmit(e){
+    
+    e.preventDefault();
+
+    const text = document.getElementById('itemText').value.trim();
+
+    const res = await fetch('http://localhost:3030/jsonstore/advanced/dropdown', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({text})
+    });
+
+    document.getElementById('itemText').value = '';
+
+    if(res.ok){
+        getData();
+    }
+}
+
+function update(items){
+
+    render(selectTemplate(items), selectEl);
 }
--- Client_Side_Rendering/05.Table-Search/solution.js
+++ Client_Side_Rendering/05.Table-Search/solution.js
@@ -1,8 +1,46 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from '../node_modules/lit-html/lit-html.js';
 
-   function onClick() {
-      //   TODO:
+const tbody = document.querySelector('tbody');
 
+document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+let data = null;
+
+const template = (student) => html `
+
+   <tr class="${student.match ? 'select' : ''}">
+      <td>${student.item.firstName} ${student.item.lastName}</td>
+      <td>${student.item.email}</td>
+      <td>${student.item.course}</td>
+   </tr>
+`
+loadData();
+
+async function loadData(){
+
+   const res = await fetch('http://localhost:3030/jsonstore/advanced/table');
+
+   if(res.ok){
+
+      data = Object.values(await res.json()).map(x => ({ item: x, match: false }))
+      
+      update();
    }
+}
+
+function update(){
+
+   render(data.map(template), tbody);
+}
+
+function onClick(){
+
+   const input = document.getElementById('searchField').value;
+
+   for (const item  of data) {
+      
+      item.match = Object.values(item.values).some(x => x.toLowerCase().includes(input));
+   }
+
+   update();
 }
